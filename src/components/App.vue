4cb202d79b03e427f34aa39f7703a3cb74158746
Burricornio <alejandro_web@hotmail.com>
2017-11-21 21:18:16 +0100

    completamos la lista de dias del calendario con los necesarios para completar la cuadricula con dias del mes previo y del mes siguiente

--- src/components/App.vue
+++ src/components/App.vue
@@ -13,12 +13,38 @@ export default {
     },
     computed: {
         days() {
+
+            // Generamos todos los días en el mes actual
             let days = [];
             let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
             do {
                 days.push(currentDay);
                 currentDay = this.$moment(currentDay).add(1, 'days');
             } while ( (currentDay.month() + 1)  === this.month );
+
+            // Añadimos los dias previos al principio del calendario
+            currentDay = this.$moment(days[0]);
+
+            const SUNDAY = 0;
+            const MONDAY = 1;
+
+            if (currentDay.day() !== MONDAY) {
+                do {
+                    currentDay = this.$moment(currentDay).subtract(1, 'days');
+                    days.unshift(currentDay);
+                } while (currentDay.day() !== MONDAY);
+            }
+
+            // Añadimos los primeros dias del siguiente mes al final del calendario
+            currentDay = this.$moment(days[days.length - 1]);
+
+            if (currentDay.day() !== SUNDAY) {
+                do {
+                    currentDay = this.$moment(currentDay).add(1, 'days');
+                    days.push(currentDay);
+                } while (currentDay.day() !== SUNDAY);
+            }
+            
             return days;
         }
     }

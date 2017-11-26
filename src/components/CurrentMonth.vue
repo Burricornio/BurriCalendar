d6af962004af2ab774157d4f5e7dfaa7fef9b35a
Burricornio <alejandro_web@hotmail.com>
2017-11-26 18:57:57 +0100

    Mostramos el mes y el año actual en el componente 'current-month'

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,9 +1,19 @@
 <template>
-  
+    <div>{{ formattedDate }}</div>
 </template>
 <script>
 export default {
-  
+    computed: {
+        month() {
+            return this.$store.state.currentMonth;
+        },
+        year() {
+            return this.$store.state.currentYear;
+        },
+        formattedDate() {
+            return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+        }
+    }
 }
 </script>
 

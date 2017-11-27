36234e0d6752cc55b13c84853348e3db9baf8440
Burricornio <alejandro_web@hotmail.com>
2017-11-27 20:59:22 +0100

    Mostrando eventos en el componente 'CalendarDay'

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,11 +1,24 @@
 <template>
-    <div :class="classObject" @click="captureClick">{{ day.format('D') }}</div>
+    <div :class="classObject" @click="captureClick">
+        {{ day.format('D') }}
+        <ul class="event-list">
+            <li v-for="(event, index) in events" :key="index">{{ event.description }}</li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {
   props: ['day'],
 
   computed: {
+      events() {
+          let mockData = [
+              { description: 'Random Event 1', date: this.$moment('2017-11-07', 'YYYY-MM-DD') },
+              { description: 'Random Event 2', date: this.$moment('2017-11-10', 'YYYY-MM-DD') },
+              { description: 'Random Event 3', date: this.$moment('2017-11-27', 'YYYY-MM-DD') }
+          ]
+          return mockData.filter(event => event.date.isSame(this.day, 'day'));
+      },
       classObject() {
           let today = this.day.isSame(this.$moment(), 'day');
           return {

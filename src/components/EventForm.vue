f5cec9ffb8893a37eb32cc0a7901e4dba678fb58
Burricornio <alejandro_web@hotmail.com>
2017-12-03 10:16:54 +0100

    Creamos una directiva customizada para cuando el input esta en estado focus y añadimos un evento '@keyup.enter' para guardar el evento al pulsar el intro del teclado

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
   <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
       <h4>Add an event</h4>
       <div class="text">
-          <input type="text" v-model="description" placeholder= "Dinner at Pancho's">
+          <input v-focus type="text" v-model="description" placeholder= "Dinner at Pancho's" @keyup.enter="create">
       </div>
       <div class="buttons">
           <button @click="create">Create</button>
@@ -39,6 +39,13 @@ export default {
         },
         close() {
             this.$store.commit('eventFormActive', false);
+        }
+    },
+    directives: {
+        focus: {
+            update(el) {
+                el.focus();
+            }
         }
     }
 }

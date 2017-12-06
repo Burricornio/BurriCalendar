bc978351d6e41ed8929f4a8f5c5c90568753a3b2
Burricornio <alejandro_web@hotmail.com>
2017-12-06 11:19:31 +0100

    Movemos los eventos fake al index.html

--- src/web.entry.js
+++ src/web.entry.js
@@ -4,11 +4,17 @@ import './style.scss';
 // Importamos el store de vuex
 import store from './store';
 
-let events = [
-  { description: 'Random Event 1', date: moment('2017-11-07', 'YYYY-MM-DD') },
-  { description: 'Random Event 2', date: moment('2017-11-10', 'YYYY-MM-DD') },
-  { description: 'Random Event 3', date: moment('2017-11-27', 'YYYY-MM-DD') }
-]
+// let events = [
+//   { description: 'Random Event 1', date: moment('2017-11-07', 'YYYY-MM-DD') },
+//   { description: 'Random Event 2', date: moment('2017-11-10', 'YYYY-MM-DD') },
+//   { description: 'Random Event 3', date: moment('2017-11-27', 'YYYY-MM-DD') }
+// ]
+let events = window.__INITIAL_STATE__.map(event =>{
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+})
 let initialState = Object.assign( {}, store.state, { events });
 store.replaceState(initialState);
 

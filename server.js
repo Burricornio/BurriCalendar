bec2f2d6b41edc79a2c527505c2ae921b1c8c9e5
Burricornio <alejandro_web@hotmail.com>
2017-12-06 11:37:11 +0100

    Añadimos mock data al index.html

--- server.js
+++ server.js
@@ -5,16 +5,24 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+let events = [
+  { description: 'Random Event 1', date: moment('2017-11-07', 'YYYY-MM-DD') },
+  { description: 'Random Event 2', date: moment('2017-11-10', 'YYYY-MM-DD') },
+  { description: 'Random Event 3', date: moment('2017-11-27', 'YYYY-MM-DD') }
+]
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->';
+  res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`));
 
 });
-
-let events = []
 
 app.use(require('body-parser').json());
 app.post('/add_event', (req, res) => {
